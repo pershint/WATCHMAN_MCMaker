e10f4866849677be2d755f7648519f90d56408d2
Teal <pershint@github.com>
2018-02-25 20:29:19 -0500

    FitEngine working, but can't find good initial conditions to fit to MC data yet

--- lib/FitterTools.py
+++ lib/FitterTools.py
@@ -1,30 +1,51 @@
 #Here are our fitter tools
 import ROOT
+import numpy as np
 
 class FitEngine(object):
-    __dlib = {"Exp":[3.0,1.0,3.0], "Gauss":[1.0,1.0,1.0], "Wald":[1000000,0, 1000000.0, 1000.0],  "Landau": [100000.0, 100000.0, 1000.0]}
+    __dlib = {"Exp":[3.0,1.0,3.0], "Gauss":[1.0,1.0,1.0], "Wald":[10,0, 10.0, 10.0,1.0],  "Landau": [1000.0, 100.0, 100.0]}
 
-    __plib = [    def __init__(self, TH1=None, distribution=None):
+    def __init__(self, TH1=None, distribution=None,fit_range=[0.0,100.0]):
         self.histogram = TH1
-        self.distribution = None
-        self.initial_parameters = None
+        self.distribution = distribution
+        if distribution is not None:
+            self.initial_parameters = self.__dlib[distribution]
+        else:
+            self.initial_parameters = None
         self.current_fit = None
+        self.fit_range = fit_range
+
+    def DrawHistogram(self):
+        if self.histogram is None:
+            print("No histogram available to draw right now.")
+        else:
+            self.histogram.Draw()
 
     def DrawFit(self):
         if self.current_fit is None:
-            print("No fit available to draw.  Run MakeTimeFit() first.")
+            print("No fit available to draw.  Run MakeFit() first.")
         else:
             self.histogram.Draw()
             self.current_fit.Draw("same")
 
+    def setHistogramToFit(self,TH1):
+        self.histogram = TH1
+
+    def setFitRange(self,fitrange):
+        if len(fitrange) is not 2:
+            print("Please give an array with two elements [min,max]")
+        if fitrange[0] >= fitrange[1]:
+            print("Please choose a min less than the max value")
+        self.fit_range = fitrange
+
     def setDistribution(self, distribution):
-        if distribution not in __dlib:
+        if distribution not in self.__dlib:
             print("Function to fit not supported.  Please select from: ")
-            print(__dlib)
+            print(self.__dlib)
             return
         else:
             self.distribution = distribution
-            self.initial_parameters = __dlib[distribution] #Default
+            self.initial_parameters = self.__dlib[distribution] #Default
 
     def setParameters(self, parameters):
         if self.initial_parameters is None:
@@ -55,39 +76,50 @@ class FitEngine(object):
         return  p[0] * (np.exp(-x[0]/p[1]) - np.exp(-x[0]/p[2]))
         #return p[0] * np.exp(-p[1] * x[0]) * (1.0 - sps.erf(((p[2]**2 / p[1])-x[0])/(np.sqrt(2)*p[2])))
     
-    def MakeTimeFit(self):
+    def MakeFit(self):
         #Function takes in a time histogram and fits a polynomial to it. Returns
         #The polynomial function for random firing of values from
         if self.distribution is None:
             print("Choose a distribution to fit your histogram to.")
             return
-        if self.distribution is "Exp": 
-            TimeFit = ROOT.TF1('TimeFit', exp, 1,
-                    100, 3)
+        if self.initial_parameters is None:
+            print("No initial parameters for this distribution.")
+        if self.distribution is "Gauss": 
+            TimeFit = ROOT.TF1('TimeFit', self.gauss, self.fit_range[0],
+                    self.fit_range[1], 3)
             for j,p in enumerate(self.initial_parameters):
-                TimeFit.SetParameter(i+1,p)
+                TimeFit.SetParameter(j+1,p)
+                TimeFit.SetParNames('A','mu','sigma')
+        if self.distribution is "Exp": 
+            TimeFit = ROOT.TF1('TimeFit', self.exp, self.fit_range[0],
+                    self.fit_range[1], 3)
+            for j,p in enumerate(self.initial_parameters):
+                TimeFit.SetParameter(j+1,p)
                 TimeFit.SetParNames('A','lamb','x0')
         if self.distribution is "Landau": 
-            TimeFit = ROOT.TF1('TimeFit', landau, 1,
-                    100, 3)
+            TimeFit = ROOT.TF1('TimeFit', self.landau, self.fit_range[0],
+                    self.fit_range[1], 3)
             for j,p in enumerate(self.initial_parameters):
-                TimeFit.SetParameter(i+1,p)
+                TimeFit.SetParameter(j+1,p)
                 TimeFit.SetParNames('A','t1','t2')
         if self.distribution is "Wald": 
-            TimeFit = ROOT.TF1('TimeFit', wald, 1,
-                    100, 3)
+            TimeFit = ROOT.TF1('TimeFit', self.wald, self.fit_range[0],
+                    self.fit_range[1], 4)
             for j,p in enumerate(self.initial_parameters):
-                TimeFit.SetParameter(i+1,p)
-                TimeFit.SetParNames('lambda','mu','C')
+                TimeFit.SetParameter(j+1,p)
+                TimeFit.SetParNames('lambda','mu','C','xp')
         TimeFit.SetLineColor(2)
         TimeFit.SetLineWidth(4)
         TimeFit.SetLineStyle(2)
+        print(TimeFit)
         ROOT.gStyle.SetOptFit(0157)
-        timehist.Fit('TimeFit','Lq')
+        self.histogram.Fit('TimeFit','Lq')
+        self.current_fit = TimeFit
         return TimeFit
    
 class ChainShooter(object):
     def __init__(self,datafiles=None,variable_list=None):
+        print("NEED TO MAKE")
 
     def MakeTH1(tree,branch):
         #Return a TH1 histogram made of all data for a branch in the given
--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 #shot using the data files given into the program.
 
 import ROOT
+from ROOT import gDirectory
 import glob
 import numpy as np
+import lib.FitterTools as ft
 import time
 import os
 
@@ -17,6 +19,26 @@ if __name__ == '__main__':
         print(f)
         ch.Add(f)
     print(ch)
-    ch.Draw("good_pos")
-    time.sleep(10)
+    ch.Draw("good_pos>>h1_good_pos(100,0.1,1.0)","","goff")
+    h1_good_pos = gDirectory.Get("h1_good_pos")
+    gfitter = ft.FitEngine(TH1=h1_good_pos,distribution="Gauss")
+    gfitter.setFitRange([0.1,1.0])
+    gfitter.setParameters([10000.0,0.5,0.5])
+    gfitter.MakeFit()
+    gfitter.DrawFit()
+    time.sleep(4)
+    ch.Draw("good_dir>>h1_good_dir(100,0.1,1.0)","","goff")
+    h1_good_dir = gDirectory.Get("h1_good_dir")
+    gfitter.setHistogramToFit(h1_good_dir)
+    gfitter.MakeFit()
+    gfitter.DrawFit()
+    time.sleep(4)
+    ch.Draw("pe>>h1_pe(300,5.0,100.0)","","goff")
+    h1_pe = gDirectory.Get("h1_pe")
+    gfitter.setHistogramToFit(h1_pe)
+    gfitter.setFitRange([0.0,100.0])
+    gfitter.setDistribution("Landau")
+    gfitter.MakeFit()
+    gfitter.DrawFit()
+    time.sleep(4)
     print("HERE WE GO")
